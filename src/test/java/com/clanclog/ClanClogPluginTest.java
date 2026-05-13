7eb5d4dec8b5a37c58c7edc975601f10b4e46455
dylan <dylan@420kc.live>
2026-05-13 11:27:39 -0500

    clan-clog: invert combined-launch dependency. ClanClogPluginTest now tryLoads "com.killclog.KillClogPlugin" from the kcpdev jar on the test classpath (mirroring kcpdev's pattern for sister plugins) and build.gradle adds a conditional testImplementation for ../kcpdev/build/libs/kill-clog-plugin-1.4.0.jar. running ./gradlew run from clan-clog now boots both plugins side by side in one runelite session; kcpdev stays clean (no temp patches to revert). when kcpdev's version bumps past 1.4.0, the file() guard misses gracefully and the launch falls back to clan-clog only.

--- src/test/java/com/clanclog/ClanClogPluginTest.java
+++ src/test/java/com/clanclog/ClanClogPluginTest.java
@@ -2,13 +2,30 @@ package com.clanclog;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
+import net.runelite.client.plugins.Plugin;
 
 public class ClanClogPluginTest
 {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ClanClogPlugin.class);
+		java.util.List<Class<? extends Plugin>> plugins = new java.util.ArrayList<>();
+		plugins.add(ClanClogPlugin.class);
+		tryLoad(plugins, "com.killclog.KillClogPlugin");
+		ExternalPluginManager.loadBuiltin(plugins.toArray(new Class[0]));
 		RuneLite.main(args);
+	}
+
+	private static void tryLoad(java.util.List<Class<? extends Plugin>> list, String className)
+	{
+		try
+		{
+			@SuppressWarnings("unchecked")
+			Class<? extends Plugin> cls = (Class<? extends Plugin>) Class.forName(className);
+			list.add(cls);
+		}
+		catch (ClassNotFoundException ignored)
+		{
+		}
 	}
 }
